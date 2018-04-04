cddf9d2431fe97122b5fde8f9261ab155c97cbf2
shakandrew <shakandrew@mail.ru>
2018-04-05 01:44:15 +0200

    Timer2 rewritten, namespace TMR2

--- SAMPLE_2_4/timer2.h
+++ SAMPLE_2_4/timer2.h
@@ -1,41 +1,54 @@
+namespace TMR2{
+    volatile int TPL; // Timer period limit
+    volatile int TPV; // Timer period value
+    volatile char TPS; // Timer period sign
+    void (*action)();
 
-#define TMR2_SEC_MAX 100
-#define TMR2_MILL_MAX 1000
+    // public functions
+    void set(short period, void (*f)());
+    void start();
+    void finish();
+    void init();
+    // private functions
+    void inc();
 
-typedef struct {
-    short mill;
-    short sec;
-    short period;
-} TMR2;
 
-void initTMR(volatile TMR2& tmr);
-void incTMR(volatile TMR2& tmr);
-void setTMR(TMR2& tmr, short sec, short mill);
+    void start() {
+        TCNT2 = 0;
+        sei();
+    }
 
-void initTMR(volatile TMR2& tmr) {
-    tmr.sec = 0;
-    tmr.mill = 0;
-    tmr.period = 10;
+    void finish() {
+        cli();
+    }
 
-    TCCR2A |= _BV(WGM21);
-    OCR2A = 0x10;
-    TIMSK2 |= _BV(OCIE2A);
-    TCCR2B |= _BV(CS22);
-}
+    void init(){
+      TCCR2A |= _BV(WGM21); // Set CTC mode
+      OCR2A = 0xF9; // Interuption value
+      TIMSK2 |= _BV(OCIE2A); // Allow interuptions, when TCNT2 == OCR2A
+      TCCR2B |= _BV(CS22); // Set prescaler for 64
+    }
 
-void incTMR(volatile TMR2& tmr) {
-    tmr.mill += tmr.period;
-    tmr.sec += tmr.mill/TMR2_MILL_MAX;
-    tmr.mill %= TMR2_MILL_MAX;
-    tmr.sec %= TMR2_SEC_MAX;
-    if (tmr.mill < 0)
-        tmr.mill = TMR2_MILL_MAX + tmr.mill;
-    if (tmr.sec < 0)
-        tmr.sec = TMR2_SEC_MAX + tmr.sec;
-}
+    void set(short period, void (*f)()) {
+        TPV = 0;
+        TPL = period;
+        action = f;
 
-void setTMR(TMR2& tmr, short sec, short mill, short period) {
-    tmr.sec = sec;
-    tmr.mill = mill;
-    tmr.period = period;
+        if (TPL>0)
+            TPS = 1;
+        else
+            TPS = -1;
+    }
+
+    void inc() {
+        TPV += TPS;
+        if (TPV == TPL) {
+            TPV = 0;
+            action();
+        }
+    }
+
+    void ISR(TIMER2_COMPA_vect) {
+        inc();
+    }
 }
