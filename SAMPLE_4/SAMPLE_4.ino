fc905ed0e21636c241499ba64e439bdda2b706c0
shakandrew <shakandrew@github.com>
2018-05-21 14:40:34 +0200

    Small refactoring / renaming vars

--- SAMPLE_4/SAMPLE_4.ino
+++ SAMPLE_4/SAMPLE_4.ino
@@ -1,10 +1,7 @@
-//#include <rd02.h>
-
 #include <Time.h>
 #include <TimeLib.h>
 #include <stdint.h>
 #include <LCD03.h>
-
 #include "MOVE_RD02.h"
 #include "TMR1.h"
 
@@ -13,8 +10,8 @@
 #define REFRESH_LIMIT 3000
 #define WHEEL_RADIUS 0.048
 #define TO_RADS(n) (n * M_PI / 180)
-#define INTER_PERIOD 1
-#define CALL_FREQUENCY (double)INTER_PERIOD / 1000 
+#define timer_PERIOD 1
+#define CALL_FREQUENCY (double)timer_PERIOD / 1000
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 #define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
 
@@ -23,24 +20,22 @@ MOVE_RD02 motor(RD02_I2C_ADDRESS);
 LCD03 lcd;
 uint16_t refresh_lcd = 0;
 
-// TODO change vars in the interuption
+volatile int32_t m1_ticks_prev;
+volatile int32_t m1_ticks_curr;
 
-volatile int32_t m1_ticks_prev=0;
-volatile int32_t m1_ticks_curr=0;
+volatile int32_t m2_ticks_prev;
+volatile int32_t m2_ticks_curr;
 
-volatile int32_t m2_ticks_prev=0;
-volatile int32_t m2_ticks_curr=0;
-
-char lcd_display_mdg[32] =
+char lcd_display_msg[32] =
     { 'M', '1', ':', ' ', '.', ' ', ' ', ' ', 'H', 'H', ':', 'M', 'M', ':', 'S', 'S',
       'M', '2', ':', ' ', '.', ' ', ' ', ' ', 'D', 'D', '/', 'M', 'M', '/', 'Y', 'Y' };
 
-volatile uint32_t inter=0;
-uint32_t inter_start_ts=0;
-uint32_t inter_end_ts=0;
+volatile uint32_t timer;
+uint32_t timer_prev;
+uint32_t timer_curr;
 
 void looper() {
-    inter++;
+    timer++;
 }
 
 void setup()
@@ -62,9 +57,11 @@ void setup()
 
     m1_ticks_curr = 0;
     m2_ticks_curr = 0;
+    timer_curr = 0;
+    timer = 0;
     // lcd cant work in interuptions
     TMR1::init();
-    TMR1::set(INTER_PERIOD, looper);
+    TMR1::set(timer_PERIOD, looper);
     TMR1::start();
 }
 
@@ -76,7 +73,7 @@ void loop()
             processSyncMessage();
     }
     else
-    {   
+    {
         if (Serial.available())
             command();
         else
@@ -90,12 +87,12 @@ void refreshData()
     if (refresh_lcd == 0)
     {
         {
-            inter_start_ts = inter_end_ts;
-            inter_end_ts = inter;
-            
+            timer_prev = timer_curr;
+            timer_curr = timer;
+
             m1_ticks_prev = m1_ticks_curr;
             m1_ticks_curr = motor.getENC1();
-                
+
             m2_ticks_prev = m2_ticks_curr;
             m2_ticks_curr = motor.getENC2();
         }
@@ -104,44 +101,44 @@ void refreshData()
             float speed2 = getSpeed( MAX(m2_ticks_prev, m2_ticks_curr), MIN(m2_ticks_prev, m2_ticks_curr) );
 
             if (m1_ticks_curr>m1_ticks_prev)
-                lcd_display_mdg[3] = '+';
+                lcd_display_msg[3] = '+';
             if (m1_ticks_curr<m1_ticks_prev)
-                lcd_display_mdg[3] = '-';
+                lcd_display_msg[3] = '-';
             if (m1_ticks_curr==m1_ticks_prev)
-                lcd_display_mdg[3] = '0';
-                
-            lcd_display_mdg[5] = getDigitFromDouble(speed1, -1);
-            lcd_display_mdg[6] = getDigitFromDouble(speed1, -2);
+                lcd_display_msg[3] = '0';
 
-            lcd_display_mdg[8] = '0' + hour() / 10;
-            lcd_display_mdg[9] = '0' + hour() % 10;
+            lcd_display_msg[5] = getDigitFromDouble(speed1, -1);
+            lcd_display_msg[6] = getDigitFromDouble(speed1, -2);
 
-            lcd_display_mdg[11] = '0' + minute() / 10;
-            lcd_display_mdg[12] = '0' + minute() % 10;
+            lcd_display_msg[8] = '0' + hour() / 10;
+            lcd_display_msg[9] = '0' + hour() % 10;
 
-            lcd_display_mdg[14] = '0' + second() / 10;
-            lcd_display_mdg[15] = '0' + second() % 10;
+            lcd_display_msg[11] = '0' + minute() / 10;
+            lcd_display_msg[12] = '0' + minute() % 10;
+
+            lcd_display_msg[14] = '0' + second() / 10;
+            lcd_display_msg[15] = '0' + second() % 10;
 
             if (m2_ticks_curr>m2_ticks_prev)
-                lcd_display_mdg[19] = '+';
+                lcd_display_msg[19] = '+';
             if (m2_ticks_curr<m2_ticks_prev)
-                lcd_display_mdg[19] = '-';
+                lcd_display_msg[19] = '-';
             if (m2_ticks_curr==m2_ticks_prev)
-                lcd_display_mdg[19] = '0';
-                
-            lcd_display_mdg[21] = getDigitFromDouble(speed2, -1);
-            lcd_display_mdg[22] = getDigitFromDouble(speed2, -2);
+                lcd_display_msg[19] = '0';
 
-            lcd_display_mdg[24] = '0' + day() / 10;
-            lcd_display_mdg[25] = '0' + day() % 10;
+            lcd_display_msg[21] = getDigitFromDouble(speed2, -1);
+            lcd_display_msg[22] = getDigitFromDouble(speed2, -2);
 
-            lcd_display_mdg[27] = '0' + month() / 10;
-            lcd_display_mdg[28] = '0' + month() % 10;
+            lcd_display_msg[24] = '0' + day() / 10;
+            lcd_display_msg[25] = '0' + day() % 10;
 
-            lcd_display_mdg[30] = '0' + (year() / 10) % 10;
-            lcd_display_mdg[31] = '0' + year() % 10;
+            lcd_display_msg[27] = '0' + month() / 10;
+            lcd_display_msg[28] = '0' + month() % 10;
 
-            lcd.print(lcd_display_mdg);
+            lcd_display_msg[30] = '0' + (year() / 10) % 10;
+            lcd_display_msg[31] = '0' + year() % 10;
+
+            lcd.print(lcd_display_msg);
         }
     }
 }
@@ -168,7 +165,7 @@ void processSyncMessage()
     setTime(pctime); // Sync Arduino clock to the time received on the serial port
 
     lcd.home();
-    lcd.print(lcd_display_mdg);
+    lcd.print(lcd_display_msg);
 }
 
 void command()
@@ -177,7 +174,7 @@ void command()
         const char* str = temp.c_str();
         Serial.print("Accepted string: ");
         Serial.println(str);
-        
+
         if (strcmp(str, "forward\0") == 0)
         {
             motor.moveForward();
@@ -240,28 +237,27 @@ void command()
         }
         if (strcmp(str, "data\0") == 0)
         {
-            Serial.print("inter_start_ts: ");
-            Serial.println(inter_start_ts);
-            Serial.print("inter_end_ts: ");
-            Serial.println(inter_end_ts);
-            
+            Serial.print("timer_prev: ");
+            Serial.println(timer_prev);
+            Serial.print("timer_curr: ");
+            Serial.println(timer_curr);
+
             Serial.print("m1_ticks_prev: ");
             Serial.println(m1_ticks_prev);
             Serial.print("m1_ticks_curr: ");
-            Serial.println(m1_ticks_curr); 
+            Serial.println(m1_ticks_curr);
             Serial.print("motor.getENC1(): ");
-            Serial.println(motor.getENC1());                       
+            Serial.println(motor.getENC1());
         }
         */
 }
 
-// TODO change to normal one
 float getSpeed(int32_t bigger, int32_t smaller)
 {
-    float delta_ts = (inter_end_ts - inter_start_ts) * CALL_FREQUENCY;
+    float delta_ts = (timer_curr - timer_prev) * CALL_FREQUENCY;
 
     float delta = 0;
-    
+
     if ( ( (uint32_t)bigger - (uint32_t)smaller) > INT32_MAX )
         delta = (INT32_MAX - bigger) + (smaller - INT32_MIN);
     else
